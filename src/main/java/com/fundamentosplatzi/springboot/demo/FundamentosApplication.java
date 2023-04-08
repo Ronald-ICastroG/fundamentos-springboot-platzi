6d4742fb88ac53fe1f71de27dc20a24b32da194e
Ronald-ICastroG_Acer <ronaldivan.1210868@correo.uis.edu.co>
2023-04-07 19:49:27 -0500

    Logs ep16

--- src/main/java/com/fundamentosplatzi/springboot/demo/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/demo/FundamentosApplication.java
@@ -4,6 +4,9 @@ import com.fundamentosplatzi.springboot.demo.bean.MyBean;
 import com.fundamentosplatzi.springboot.demo.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.demo.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.demo.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.demo.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +15,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
+
+	private	final Log LOGGER= LogFactory.getLog(FundamentosApplication.class);
+
 	private MyBean myBean;
 	private ComponentDependency componentDependency;
 
@@ -19,12 +25,16 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	private MyBeanWithProperties myBeanWithProperties;
 
+	private UserPojo userPojo;
+
 	@Autowired //ya en versiones recientes no es obligatorio
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean,MyBeanWithDependency myBeanWithDependency,MyBeanWithProperties myBeanWithProperties){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean,MyBeanWithDependency myBeanWithDependency,MyBeanWithProperties myBeanWithProperties,UserPojo userPojo){
 		this.componentDependency=componentDependency;
 		this.myBean=myBean;
 		this.myBeanWithDependency=myBeanWithDependency;
 		this.myBeanWithProperties=myBeanWithProperties;
+		this.userPojo=userPojo;
+
 	}
 
 
@@ -38,5 +48,15 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
+
+		try {
+			//error
+			int value=10/0;
+			LOGGER.debug("mi valor "+value);
+		} catch(Exception e) {
+			LOGGER.error("Esto es un error al dividir por cero "+e.getMessage());
+
+	}
 	}
 }
--- src/main/java/com/fundamentosplatzi/springboot/demo/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/demo/configuration/GeneralConfiguration.java
@@ -2,11 +2,15 @@ package com.fundamentosplatzi.springboot.demo.configuration;
 
 import com.fundamentosplatzi.springboot.demo.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.demo.bean.MyBeanWithPropertiesImplement;
+import com.fundamentosplatzi.springboot.demo.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
+
 public class GeneralConfiguration {
 
     @Value("${value.name}")
